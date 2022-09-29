6491617d82fde43b93c2259953f92c95cece781d
Yogesh <yogeshborra058@gmail.com>
2022-09-29 15:33:14 +0530

    3rd commit

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,3 +1,4 @@
+const shortid = require('shortid')
 const shortId = require('shortid')
 const validUrl = require('valid-url')
 const urlModel = require("../model/urlModel")
@@ -8,7 +9,11 @@ const urlShortner = async function (req, res) {
         let { longUrl } = req.body
         if (!longUrl) return res.status(400).send({ status: false, msg: "provide url" })
         if (!validUrl.isUri(longUrl)) return res.status(400).send({ status: false, msg: "invalid url" })
-        let urlCode = shortId.generate()
+        let findUrl  = await urlModel.findOne({longUrl: longUrl}).select({longUrl : 1 , urlCode : 1 , shortUrl : 1 , _id : 0});
+        if(findUrl){
+            return res.status(409).send({message : "This URL has already being shortened" , data : findUrl})
+        }
+        let urlCode = shortId.generate().toLowerCase()
         req.body.urlCode = urlCode
         const baseUrl = "http://localhost:3000"
         if (!validUrl.isUri(baseUrl)) return res.status(400).send({ status: false, msg: "invalid baseUrl" })
@@ -20,7 +25,27 @@ const urlShortner = async function (req, res) {
     }
     catch (error) {
         return res.status(500).send({ status: false, msg: error.message })
-
     }
 }
-module.exports = { urlShortner }
+
+const getUrl = async function(req , res){
+    try{
+        let urlcode = req.params.urlCode;
+        if(!shortid.isValid(urlcode)){
+            return res.status(400).send({ status: false, msg: "invalid urlcode" })
+        }
+        let url = await urlModel.findOne({urlCode : urlcode});
+        if(!url){
+            return res
+        .status(404)
+        .send({ status: false, msg: "url Document not Found" })
+
+        }
+        return res.status(200).redirect(url.longUrl)
+
+    }catch (error) {
+        return res.status(500).send({ status: false, msg: error.message })
+    }
+}
+
+module.exports = { urlShortner , getUrl }
--- src/route/routes.js
+++ src/route/routes.js
@@ -6,17 +6,7 @@ const urlController=require("../controller/urlController")
 
 router.post('/url/shorten',urlController.urlShortner)
 
-
-
-
-
-
-
-
-
-
-
-
+router.get("/:urlCode" , urlController.getUrl)
 
 router.all("*", function (req, res) {
     res.status(404).send({
