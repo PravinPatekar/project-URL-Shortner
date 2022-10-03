7bbbb783b8a54ece4cd5b042483dd3f8225c87e7
kamrebaba <alamkamre58@gmail.com>
2022-10-03 17:06:06 +0530

    redis work done

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,8 +1,34 @@
-const shortid = require('shortid')
+ const shortid = require('shortid')
 const shortId = require('shortid')
 const validUrl = require('valid-url')
 const urlModel = require("../model/urlModel")
+const redis = require('redis')
+const {promisify}= require("util")
 
+//Connect to redis
+const redisClient = redis.createClient(
+    13631,
+    "redis-13631.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("wyyIMitZQ2OeeYVP1arHZjBze790bVNX", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+  });
+  
+  
+  
+  //1. connect to the server
+  //2. use the commands :
+  
+  //Connection setup for redis
+  
+  const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+  const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+  
 
 const urlShortner = async function (req, res) {
     try {
@@ -21,6 +47,7 @@ const urlShortner = async function (req, res) {
         console.log(shortUrl)
         req.body.shortUrl = shortUrl
         const data = await urlModel.create(req.body)
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(data));
         return res.status(201).send({ status: true, msg:req.body })
     }
     catch (error) {
@@ -30,18 +57,26 @@ const urlShortner = async function (req, res) {
 
 const getUrl = async function(req , res){
     try{
-        let urlcode = req.params.urlCode;
-        if(!shortid.isValid(urlcode)){
+        let urlCode = req.params.urlCode;
+        if(!shortid.isValid(urlCode)){
             return res.status(400).send({ status: false, msg: "invalid urlcode" })
         }
-        let url = await urlModel.findOne({urlCode : urlcode});
+        const cachedUrl= await GET_ASYNC(`${urlCode}`)
+        const parseUrl=JSON.parse(cachedUrl)
+        if(parseUrl){
+            console.log(parseUrl)
+            return res.status(302).redirect(parseUrl.longUrl)
+        }
+        console.log("roger-roger")
+        let url = await urlModel.findOne({urlCode : urlCode});
         if(!url){
             return res
         .status(404)
         .send({ status: false, msg: "url Document not Found" })
 
         }
-        return res.status(200).redirect(url.longUrl)
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(url));
+        return res.status(302).redirect(url.longUrl)
 
     }catch (error) {
         return res.status(500).send({ status: false, msg: error.message })
