272b26dc8dadfec3a05b0b05b32677e2b0802791
Yogesh <yogeshborra058@gmail.com>
2022-10-06 09:51:09 +0530

    Final commit

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -28,27 +28,27 @@ const urlShortner = async function (req, res) {
     try {
         let longUrl = req.body.longUrl
         if (!longUrl) {
-            return res.status(400).send({ status: false, msg: "Provide url" })
+            return res.status(400).send({ status: false, message: "Provide URL" })
         }
         if (!validUrl.isUri(longUrl.trim())) {
-            return res.status(400).send({ status: false, msg: "Invalid url" })
+            return res.status(400).send({ status: false, message: "Invalid URL" })
         }
+        req.body.longUrl = longUrl.trim()
         let findUrl = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, urlCode: 1, shortUrl: 1, _id: 0 });
         if (findUrl) {
-            return res.status(409).send({ message: "This URL has already being shortened", data: findUrl })
+            return res.status(200).send({ message: "This URL has already being shortened", data: findUrl })
         }
         let urlCode = shortid.generate().toLowerCase()
         req.body.urlCode = urlCode
         const baseUrl = "http://localhost:3000"
         if (!validUrl.isUri(baseUrl)) {
-            return res.status(400).send({ status: false, msg: "invalid baseUrl" })
+            return res.status(400).send({ status: false, message: "Invalid BaseUrl" })
         }
         const shortUrl = baseUrl + '/' + urlCode
-        console.log(shortUrl)
         req.body.shortUrl = shortUrl
         const data = await urlModel.create(req.body)
         await SET_ASYNC(`${urlCode}`, JSON.stringify(data));
-        return res.status(201).send({ status: true, msg: req.body })
+        return res.status(201).send({ status: true, data: req.body })
     }
     catch (error) {
         return res.status(500).send({ status: false, msg: error.message })
@@ -61,7 +61,7 @@ const redirectingUrl = async function (req, res) {
     try {
         let urlCode = req.params.urlCode;
         if (!shortid.isValid(urlCode)) {
-            return res.status(400).send({ status: false, msg: "invalid urlcode" })
+            return res.status(400).send({ status: false, message: "Invalid urlcode" })
         }
         const cachedUrl = await GET_ASYNC(`${urlCode}`)
         const parseUrl = JSON.parse(cachedUrl)
@@ -73,13 +73,13 @@ const redirectingUrl = async function (req, res) {
         if (!url) {
             return res
                 .status(404)
-                .send({ status: false, msg: "url Document not Found" })
+                .send({ status: false, msg: ` Document with this Urlcode:${urlCode} not Found..` })
         }
         await SET_ASYNC(`${urlCode}`, JSON.stringify(url));
         return res.status(302).redirect(url.longUrl)
 
     } catch (error) {
-        return res.status(500).send({ status: false, msg: error.message })
+        return res.status(500).send({ status: false, message: error.message })
     }
 }
 
